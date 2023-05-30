3cd69bf48d4de438fffe229eff65696b05b4d278
maryRom01 <tester.automation.11@gmail.com>
2023-05-29 22:41:08 -0400

    add service (root app injector)

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
     selector: 'pm-products',
@@ -11,8 +12,12 @@ export class ProductListComponent implements OnInit{
     imageWidth: number = 50;
     imageMargin : number = 2;
     showImage: boolean = false;
-
     private _listFilter: string = '';
+
+    constructor(private productService: ProductService) {
+   
+    }
+
     get listFilter(): string {
         return this._listFilter;
     }
@@ -23,34 +28,15 @@ export class ProductListComponent implements OnInit{
     
     filteredProducts: IProduct[] = [];
 
-    products: IProduct[] = [
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2021",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png" 
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2021",
-            "description": "Curved claw street hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png" 
-        }
-    ];
+    products: IProduct[] = [];
 
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
 
     ngOnInit(): void {
+        this.products = this.productService.getProducts();
+        this.filteredProducts = this.products;
         this.listFilter = '';
     }
 
